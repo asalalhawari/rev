b083698134b11aa4f25fe02cebf226cdc5c267bc
ASAL HAWARI <ASALHAWARI2000@gmail.com>
2024-06-20 17:35:58 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+///  review (splice)
+//splice
+// const months = ['Jan', 'March', 'April', 'June'];
+// months.splice(2, 0, 'Feb');
+// console.log(months);
+// months.splice(4, 1, 'May');
+// console.log(months);
+//بتعمل اضافة او العكس 
+//(4, 1, 'May') اول وحده بنحدد موقعها وثانيه تبلش من بعد المكان الي بدي ياه وثالثه الاضافة 
+// ـــــــــــــــــــــــــــــــــــ
+//(slice) 
+// let array = [1, 2, 3, 4, 5];
+// let slicedArray = array.slice(1, 4); 
+// console.log(slicedArray);
+//لو بدي اعمل ازاله 
+// let array = [1, 2, 3, 4, 5];
+// let slicedArray = array.slice(-3); 
+// console.log(slicedArray);
+
+
+// let str = "Hello, world";
+// let slicedStr = str.slice(7, 12); 
+// console.log(slicedStr);
+
+// let str = "Hello, world";
+// let slicedStr = str.slice(-7); 
+// console.log(slicedStr);
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+
